2486bb6ca46951e7c70e668c18b5859046771c0b
damianamodeo <damianamodeotheocratic@gmail.com>
2023-12-10 14:17:37 +1000

    tweak log and return values of firestore doc size function

--- libs/data/firebase/src/lib/getFirestoreDocumentSize.ts
+++ libs/data/firebase/src/lib/getFirestoreDocumentSize.ts
@@ -1,23 +1,24 @@
 import sizeof from 'firestore-size';
 
+export type FirestoreDocumentSize = {
+  bytes: number;
+  KB: number;
+  percent: number;
+};
+
 export const getFirestoreDocumentSize = (doc: any) => {
-  let bytes = sizeof(doc);
+  const bytes = sizeof(doc);
+  const percentage = ((bytes / 1024 / 1024) * 100).toFixed(2);
+
   if (bytes < 1024) {
-    console.log(`Document size: ${bytes} bytes`);
-    return;
-  }
-  bytes = bytes / 1024;
-  if (bytes < 1024) {
-    console.log(`Document size: ${bytes} KB`);
-    return;
-  }
-  bytes = bytes / 1024;
-  if (bytes < 1024) {
-    console.log(`Document size: ${bytes} MB`);
-    return;
+    console.log(`Document size: ${bytes} bytes (${percentage})%`);
+  } else {
+    console.log(
+      `Document size: ${(bytes / 1024).toFixed(2)} KB (${percentage})%`
+    );
   }
 
-  return bytes;
+  return { bytes: bytes, KB: (bytes / 1024).toFixed(2), percent: percentage };
 };
 
 export default getFirestoreDocumentSize;
