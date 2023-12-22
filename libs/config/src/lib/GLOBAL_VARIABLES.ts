e7017ba10f94335920af05013366563e8eaf0bdc
damianamodeo <damianamodeotheocratic@gmail.com>
2023-12-23 09:25:53 +1000

    add blank mapbox map component and tweak GLOBAL_VARIABLES.MAITLAND_COORDS

--- libs/config/src/lib/GLOBAL_VARIABLES.ts
+++ libs/config/src/lib/GLOBAL_VARIABLES.ts
@@ -2,11 +2,7 @@ export const GLOBAL_VARIABLES = {
   CONGREGATION: 'test',
   MAPBOX_API_KEY:
     'pk.eyJ1IjoiZGFtaWFuYW1vZGVvIiwiYSI6ImNqeWxnb3lsejA4OXozYmxpajhzMXdvZjQifQ.OJBOK5ZvGEX2VaScbW_zUQ',
-  MAITLAND_COORDS: {
-    lng: 151.621,
-    lat: -32.777,
-    zoom: 10,
-  },
+  MAITLAND_COORDS: [151.621, -32.777], // 59 Woodlands Drive
 };
 
 export default GLOBAL_VARIABLES;
