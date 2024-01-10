87411213f6725574061e27bc01418dd705b0b7e2
damianamodeo <damianamodeotheocratic@gmail.com>
2024-01-10 20:07:08 +1300

    housekeeping

--- libs/shells/proclaimer/src/lib/AppShell.tsx
+++ libs/shells/proclaimer/src/lib/AppShell.tsx
@@ -1,5 +1,55 @@
 import { IonicApp } from '@ui-ion';
-import content from './content';
+import { settingsOutline, homeOutline } from 'ionicons/icons';
+import { Settings, Appearance } from './settings/Index';
+import Home from './home/components/Home';
+import Record from './home/components/record/Record';
+import Return from './home/components/return/Return';
+import Write from './home/components/write/Write';
+
+const content = [
+  // HOME
+  {
+    label: 'Home',
+    component: Home,
+    isTab: true,
+    icon: homeOutline,
+    path: '/home',
+    redirect: true,
+  },
+  {
+    label: 'Record',
+    component: Record,
+    path: '/home/record',
+  },
+  {
+    label: 'Return',
+    component: Return,
+    path: '/home/return',
+  },
+  {
+    label: 'Write',
+    component: Write,
+    path: '/home/write',
+  },
+  // SETTINGS
+  {
+    label: 'Settings',
+    component: Settings,
+    isTab: true,
+    icon: settingsOutline,
+    path: '/settings',
+  },
+  {
+    label: 'Appearance',
+    component: Appearance,
+    path: '/settings/apperance',
+  },
+];
+
+export const path = content.reduce((acc, current) => {
+  acc[current.label] = current.path;
+  return acc;
+}, {} as { [key: string]: string });
 
 export const AppShell: React.FC = () => <IonicApp content={content}></IonicApp>;
 
--- libs/shells/proclaimer/src/lib/home/components/Home.tsx
+++ libs/shells/proclaimer/src/lib/home/components/Home.tsx
@@ -1,42 +1,63 @@
 import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
 } from '@ionic/react';
-import { path } from '../../content';
+import { LoadingSpinner } from '@ui-ion';
+import { Suspense } from 'react';
+import { path } from '../../AppShell';
 
 export const Home = () => {
   // TODO Reword descriptions in Card Content
   return (
-    <>
-      <IonCard routerLink={path.Record}>
-        <IonCardHeader>
-          <IonCardTitle>Record</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          Input addresses for unanswered calls. The list will be shared with
-          others for thorough and efficient follow-ups.
-        </IonCardContent>
-      </IonCard>
-      <IonCard routerLink={path.Return}>
-        <IonCardHeader>
-          <IonCardTitle>Return</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          Find addresses of homes to call on where no one was found at home.
-        </IonCardContent>
-      </IonCard>
-      <IonCard routerLink={path.Write}>
-        <IonCardHeader>
-          <IonCardTitle>Write</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          Find addresses of people that have not been found at home to send your
-          letters.
-        </IonCardContent>
-      </IonCard>
-    </>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton></IonBackButton>
+          </IonButtons>
+          <IonTitle>Home.1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <Suspense fallback={<LoadingSpinner></LoadingSpinner>}>
+          <IonCard routerLink={path.Record}>
+            <IonCardHeader>
+              <IonCardTitle>Record</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              Input addresses for unanswered calls. The list will be shared with
+              others for thorough and efficient follow-ups.
+            </IonCardContent>
+          </IonCard>
+          <IonCard routerLink={path.Return}>
+            <IonCardHeader>
+              <IonCardTitle>Return</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              Find addresses of homes to call on where no one was found at home.
+            </IonCardContent>
+          </IonCard>
+          <IonCard routerLink={path.Write}>
+            <IonCardHeader>
+              <IonCardTitle>Write</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              Find addresses of people that have not been found at home to send
+              your letters.
+            </IonCardContent>
+          </IonCard>
+        </Suspense>
+      </IonContent>
+    </IonPage>
   );
 };
 
